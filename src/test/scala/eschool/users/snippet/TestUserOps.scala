aed1af7a22967840bef16c4eea1a8bd5c748dbe3
System Administrator <sysadmin@200-202-server3.(none)>
2011-07-19 11:20:14 -0400

    got Selenium tests kinda working

--- src/test/scala/eschool/users/snippet/TestUserOps.scala
+++ src/test/scala/eschool/users/snippet/TestUserOps.scala
@@ -1,15 +1,16 @@
-package eschool.users.model
+package eschool.users.snippet
 
 import org.junit.Assert._
 import org.junit._
 import org.openqa.selenium.chrome.{ChromeDriverService, ChromeDriver}
 import org.openqa.selenium.chrome.ChromeDriverService.Builder
 import org.openqa.selenium.remote.{DesiredCapabilities, RemoteWebDriver}
-import org.openqa.selenium.{By, WebElement, WebDriver}
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 import org.scalatest.junit.{JUnitSuite, AssertionsForJUnit}
 import java.io.File
 import org.openqa.selenium.firefox.FirefoxDriver
+import com.thoughtworks.selenium.Selenium
+import org.openqa.selenium.{WebDriverBackedSelenium, By, WebElement, WebDriver}
 
 class TestUserOps extends JUnitSuite with BeforeAndAfterAll {
   val service = new ChromeDriverService.Builder()
@@ -18,6 +19,7 @@ class TestUserOps extends JUnitSuite with BeforeAndAfterAll {
     .build()
   service.start()
   var driver: WebDriver = null
+  var selenium: Selenium = null
 
   override def afterAll() {
     service.stop()
@@ -26,6 +28,7 @@ class TestUserOps extends JUnitSuite with BeforeAndAfterAll {
   @Before
   def createDriver() {
     driver = new ChromeDriver(service)
+    selenium = new WebDriverBackedSelenium(driver, "http://localhost:8080")
   }
 
   @After
@@ -34,11 +37,23 @@ class TestUserOps extends JUnitSuite with BeforeAndAfterAll {
   }
 
   @Test
-  def googleSearch() = {
-    driver.get("http://www.google.com");
-    val searchBox = driver.findElement(By.name("q"))
-    searchBox.sendKeys("webdriver")
-    searchBox.submit()
-    assertEquals("webdriver - Google Search", driver.getTitle)
+  def homepage() = {
+    selenium.open("")
+    assert(driver.getTitle === "JCPS eSchool")
+    assert(selenium.isTextPresent("Welcome to your JCPS eSchool"))
+    assert(selenium.isElementPresent("link=Login"))
+  }
+
+  @Test
+  def login1() = {
+    selenium.open("")
+    selenium.click("link=Login")
+    assert(driver.getTitle === "Login")
+    assert(selenium.isTextPresent("Login"))
+  }
+
+  def inputByLabel(text: String): WebElement = {
+    val label: WebElement = driver.findElement(By.xpath("//label@contains(text(), '%s'".format(text)))
+    driver.findElement(By.id(label.getAttribute("for")))
   }
 }
