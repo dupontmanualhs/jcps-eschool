f91928dbb20097b92a831ebc89bc2e8f361ec0e2
Todd O'Bryan <toddobryan@gmail.com>
2011-08-02 14:55:20 -0400

    Edit Page actually works now!!!

---
+++ src/main/scala/eschool/utils/snippet/EditorScreen.scala
@@ -0,0 +1,13 @@
+package eschool.utils.snippet
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sysadmin
+ * Date: 8/2/11
+ * Time: 1:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class EditorScreen {
+
+}
