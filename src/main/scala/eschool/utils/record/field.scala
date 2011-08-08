ae7b11f86a874d0726ada4af1c0242afb5cada1f
Todd O'Bryan <toddobryan@gmail.com>
2011-08-08 08:51:07 -0400

    started work on MongoRecordListField

--- src/main/scala/eschool/utils/record/field.scala
+++ src/main/scala/eschool/utils/record/field.scala
@@ -1,82 +1,78 @@
 package eschool.utils.record.field
 
-import net.liftweb.common._
+import tools.cmd.Meta
+import net.liftweb.common.{Failure, Empty, Full, Box}
+import net.liftweb.json.JsonAST.{JArray, JValue}
+import net.liftweb.record.{FieldHelpers, MandatoryTypedField, Field}
+import net.liftweb.json.JsonParser
+import xml.NodeSeq
+import net.liftweb.mongodb.record.{MongoMetaRecord, BsonRecord}
+import org.apache.xalan.xsltc.compiler.util.ObjectType
+import org.ietf.jgss.Oid
 import org.bson.types.ObjectId
-import net.liftweb.json.JsonAST._
-import net.liftweb.http.S
-import net.liftweb.http.js.JE._
-import net.liftweb.json.Printer
-import net.liftweb.record._
-import net.liftweb.util.Helpers._
-import net.liftweb.mongodb.record.BsonRecord
+import com.mongodb.{BasicDBList, DBObject}
+import net.liftweb.mongodb.record.field.{ObjectIdPk, MongoFieldFlavor}
 
-/*
-trait ObjectIdTypedField extends TypedField[ObjectId] {
-  def owner[OwnerType <: BsonRecord[OwnerType]]: OwnerType
+class MongoRecordListField[OwnerType <: BsonRecord[OwnerType], ObjType <: ObjectIdPk[ObjType]](rec: OwnerType)
+  extends Field[List[ObjType], OwnerType]
+  with MandatoryTypedField[List[ObjType]]
+  with MongoFieldFlavor[List[ObjType]]
+{
 
-    def defaultValue = ObjectId.get
+  import Meta.Reflection._
 
-  def setFromAny(in: Any): Box[ObjectId] = in match {
-    case oid: ObjectId => setBox(Full(oid))
-    case Some(oid: ObjectId) => setBox(Full(oid))
-    case Full(oid: ObjectId) => setBox(Full(oid))
-    case (oid: ObjectId) :: _ => setBox(Full(oid))
-    case s: String => setFromString(s)
-    case Some(s: String) => setFromString(s)
-    case Full(s: String) => setFromString(s)
-    case null|None|Empty => setBox(defaultValueBox)
-    case f: Failure => setBox(f)
-    case o => setFromString(o.toString)
+  def owner = rec
+
+  def defaultValue = List[ObjType]()
+
+  def setFromAny(in: Any): Box[List[ObjType]] = {
+    in match {
+      case dbo: DBObject => setFromDBObject(dbo)
+      case list: List[ObjType] => setBox(Full(list))
+      case Some(list: List[ObjType]) => setBox(Full(list))
+      case Full(list: List[ObjType]) => setBox(Full(list))
+      case s: String => setFromString(s)
+      case Some(s: String) => setFromString(s)
+      case Full(s: String) => setFromString(s)
+      case null|None|Empty => setBox(defaultValueBox)
+      case f: Failure => setBox(f)
+      case o => setFromString(o.toString)
+    }
   }
 
-  def setFromJValue(jvalue: JValue): Box[ObjectId] = jvalue match {
+  def setFromJValue(jvalue: JValue) = jvalue match {
     case JNothing|JNull if optional_? => setBox(Empty)
-    case JObject(JField("$oid", JString(s)) :: Nil) => setFromString(s)
-    case JString(s) => setFromString(s)
-    case other => setBox(FieldHelpers.expectedA("JObject", other))
+    case JArray(arr) => setBox(Full(arr.map(_.values.asInstanceOf[ObjType])))
+    case other => setBox(FieldHelpers.expectedA("JArray", other))
   }
 
-  def setFromString(in: String): Box[ObjectId] =
-    if (ObjectId.isValid(in))
-      setBox(Full(new ObjectId(in)))
-    else
-      setBox(Failure("Invalid ObjectId string: "+in))
-
-  private def elem =
-    S.fmapFunc(S.SFuncHolder(this.setFromAny(_))){funcName =>
-      <input type="text"
-        name={funcName}
-        value={valueBox.map(s => s.toString) openOr ""}
-        tabindex={tabIndex toString}/>
-    }
-
-  def toForm =
-    uniqueFieldId match {
-      case Full(id) => Full(elem % ("id" -> id))
-      case _ => Full(elem)
-    }
-
-  def asJs = asJValue match {
-    case JNothing => JsNull
-    case jv => JsRaw(Printer.compact(render(jv)))
+  // parse String into a JObject
+  def setFromString(in: String): Box[List[ObjType]] = tryo(JsonParser.parse(in)) match {
+    case Full(jv: JValue) => setFromJValue(jv)
+    case f: Failure => setBox(f)
+    case other => setBox(Failure("Error parsing String into a JValue: "+in))
   }
 
-  def asJValue: JValue = valueBox.map(v => Meta.objectIdAsJValue(v, owner.meta.formats)) openOr (JNothing: JValue)
-}
+  // TODO: This should return a multiple select form
+  def toForm: Box[NodeSeq] = Empty
 
-class ObjectIdField[OwnerType <: BsonRecord[OwnerType]](rec: OwnerType)
-  extends Field[ObjectId, OwnerType]
-  with MandatoryTypedField[ObjectId]
-  with ObjectIdTypedField
-{
-  def owner = rec
-}
+  def asJValue = JArray(value.map(li => li.asInstanceOf[AnyRef] match {
+    case x if primitive_?(x.getClass) => primitive2jvalue(x)
+    case x if mongotype_?(x.getClass) => mongotype2jvalue(x)(owner.meta.formats)
+    case x if datetype_?(x.getClass) => datetype2jvalue(x)(owner.meta.formats)
+    case _ => JNothing
+  }))
 
-class OptionalObjectIdField[OwnerType <: BsonRecord[OwnerType]](rec: OwnerType)
-  extends Field[ObjectId, OwnerType]
-  with OptionalTypedField[ObjectId]
-  with ObjectIdTypedField
-{
-  def owner = rec
+  /*
+  * Convert this field's value into a DBObject so it can be stored in Mongo.
+  */
+  def asDBObject: DBObject = {
+    new BasicDBList().addAll(value map _.id.get)
+  }
+
+  // set this field's value using a DBObject returned from Mongo.
+  def setFromDBObject(dbo: DBObject): Box[List[ObjType]] =
+    setBox(Full(dbo.asInstanceOf[BasicDBList].toList.map(
+      (oid: Object) => ObjType.find(oid.asInstanceOf[ObjectId]) openOr null))
+    )
 }
-*/
