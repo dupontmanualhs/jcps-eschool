bd74ea6a4e698c00b67894bf79939c0ce8fa474e
Zach Brewer <zbrewer02@gmail.com>
2011-09-06 14:19:16 -0400

    Got the Student schedule snippet to work

---
+++ src/main/scala/eschool/courses/snippet/StudentSchedule.scala
@@ -0,0 +1,13 @@
+package eschool.courses.snippet
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: zbrewer13
+ * Date: 9/6/11
+ * Time: 1:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class StudentSchedule {
+
+}
